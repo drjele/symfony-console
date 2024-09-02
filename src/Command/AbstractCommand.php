d5a8ce29a9ee7b211576c14f512d5043416b6ef9
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2024-09-02 16:24:20 +0300

    remove MemoryAndTimeLimitsTrait from AbstractCommand

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 namespace Drjele\Symfony\Console\Command;
 
 use DateTime;
-use Drjele\Symfony\Console\Command\Trait\MemoryAndTimeLimitsTrait;
 use Drjele\Symfony\Console\OutputStyle\Trait\SymfonyStyleTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,14 +17,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 abstract class AbstractCommand extends Command
 {
     use SymfonyStyleTrait;
-
-    /** @deprecated will be removed from here in next minor version */
-    use MemoryAndTimeLimitsTrait;
-
-    /** there are here for older symfony projects */
-    public const SUCCESS = 0;
-    public const FAILURE = 1;
-    public const INVALID = 2;
 
     protected InputInterface $input;
     protected OutputInterface $output;
@@ -36,15 +27,7 @@ abstract class AbstractCommand extends Command
         $this->output = $output;
 
         $this->initializeSymfonyStyle();
-        $this->initializeMemoryAndTimeLimits();
 
         $this->style->title(\sprintf('<bg=blue>[%s]</> %s', (new DateTime())->format('Y-m-d'), $this->getName()));
-    }
-
-    protected function configure(): void
-    {
-        parent::configure();
-
-        $this->configureMemoryAndTimeLimits();
     }
 }
